26d1765ef8e571af8766284e97329018b373f365
matdon90 <mdonhefner@gmail.com>
2020-02-14 11:11:26 +0100

    First integration with Firebase Database RealTime.

--- LpgConsumptionCostCalculator.Web/Controllers/HomeController.cs
+++ LpgConsumptionCostCalculator.Web/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using LpgConsumptionCostCalculator.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +12,37 @@ namespace LpgConsumptionCostCalculator.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            //Simulate test user data and login timestamp
+            var userId = "12345";
+            var currentLoginTime = DateTime.UtcNow.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //Save non identifying data to Firebase
+            var currentUserLogin = new LoginData() { TimeStampUtc = currentLoginTime };
+            var firebaseClient = new FirebaseClient("https://lpgconsumptioncostcalculator.firebaseio.com");
+            var result = await firebaseClient
+              .Child("Users/" + userId + "/Logins")
+              .PostAsync(currentUserLogin);
+
+            //Retrieve data from Firebase
+            var dbLogins = await firebaseClient
+              .Child("Users")
+              .Child(userId)
+              .Child("Logins")
+              .OnceAsync<LoginData>();
+
+            var timestampList = new List<DateTime>();
+
+            //Convert JSON data to original datatype
+            foreach (var login in dbLogins)
+            {
+                timestampList.Add(Convert.ToDateTime(login.Object.TimeStampUtc).ToLocalTime());
+            }
+
+            //Pass data to the view
+            ViewBag.CurrentUser = userId;
+            ViewBag.Logins = timestampList.OrderByDescending(x => x);
             return View();
         }
 
