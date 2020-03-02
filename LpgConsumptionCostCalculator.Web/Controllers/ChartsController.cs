91778b85adef52fb1db279eb5e695f1236013506
matdon90 <mdonhefner@gmail.com>
2020-03-02 14:40:48 +0100

    Trends by id. Integrateded with navigation.

--- LpgConsumptionCostCalculator.Web/Controllers/ChartsController.cs
+++ LpgConsumptionCostCalculator.Web/Controllers/ChartsController.cs
@@ -19,25 +19,31 @@ namespace LpgConsumptionCostCalculator.Web.Controllers
             this.db = db;
         }
         // GET: Charts
-        public async Task<ActionResult> Chart()
+        public async Task<ActionResult> Chart(int? id)
         {
-            var results = await db.GetAll();
-            var receiptViewModels = results.Select(vm => new FuelReceiptViewModel
+            if (id!=null)
             {
-                Id = vm.Id,
-                FueledCarId = vm.FueledCarId,
-                DistanceFromLastRefueling = vm.DistanceFromLastRefueling,
-                Comment = vm.Comment,
-                FuelAmount = vm.FuelAmount,
-                RefuelingDate = vm.RefuelingDate,
-                FuelPrice = vm.FuelPrice,
-                FuelType = vm.FuelType,
-                PetrolStationName = vm.PetrolStationName
-            });
-            ViewBag.consumptionArray = receiptViewModels.Select(r => decimal.Round(r.FuelConsumption, 2, MidpointRounding.AwayFromZero)).ToArray();
-            ViewBag.priceArray = receiptViewModels.Select(r => decimal.Round(r.PriceFor100km, 2, MidpointRounding.AwayFromZero)).ToArray();
-            ViewBag.dateArray = receiptViewModels.Select(r =>  r.RefuelingDate.ToString("dd/MM/yyyy")).ToArray();
-            return View();
+                var results = await db.GetAll();
+                var receiptViewModels = results.Where(vm => vm.FueledCarId == id).Select(vm => new FuelReceiptViewModel
+                {
+                    Id = vm.Id,
+                    FueledCarId = vm.FueledCarId,
+                    DistanceFromLastRefueling = vm.DistanceFromLastRefueling,
+                    Comment = vm.Comment,
+                    FuelAmount = vm.FuelAmount,
+                    RefuelingDate = vm.RefuelingDate,
+                    FuelPrice = vm.FuelPrice,
+                    FuelType = vm.FuelType,
+                    PetrolStationName = vm.PetrolStationName
+                });
+
+                ViewBag.consumptionArray = receiptViewModels.Select(r => decimal.Round(r.FuelConsumption, 2, MidpointRounding.AwayFromZero)).ToArray();
+                ViewBag.priceArray = receiptViewModels.Select(r => decimal.Round(r.PriceFor100km, 2, MidpointRounding.AwayFromZero)).ToArray();
+                ViewBag.dateArray = receiptViewModels.Select(r => r.RefuelingDate.ToString("dd/MM/yyyy")).ToArray();
+                return View();
+            }
+            return RedirectToAction("Index", "Cars");
+
         }
     }
 }
