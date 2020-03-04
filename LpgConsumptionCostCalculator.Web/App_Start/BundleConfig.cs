fd99bfe8e496491f13918d67ca36e42fbacc0611
Matis <Matis@DESKTOP-U37SB43>
2020-03-04 11:07:29 +0100

    Sidebar created based on sb-admin theme.

--- LpgConsumptionCostCalculator.Web/App_Start/BundleConfig.cs
+++ LpgConsumptionCostCalculator.Web/App_Start/BundleConfig.cs
@@ -42,6 +42,15 @@ namespace LpgConsumptionCostCalculator.Web
             //css
             bundles.Add(new StyleBundle("~/Content/csschartjs").Include(
                         "~/Content/Chart.min.css"));
+
+            //Create bundle for sb-admin-2
+
+            //js
+            bundles.Add(new ScriptBundle("~/bundles/sbadmin2").Include(
+                        "~/Scripts/sb-admin-2.min.js"));
+            //css
+            bundles.Add(new StyleBundle("~/Content/csssbadmin2").Include(
+                        "~/Content/sb-admin-2.min.css"));
         }
     }
 }
