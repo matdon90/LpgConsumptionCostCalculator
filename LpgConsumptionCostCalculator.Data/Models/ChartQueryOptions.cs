0da3a6a4b3d131c0f647d105f8d6d2ff8f1b7dca
matdon90 <mdonhefner@gmail.com>
2020-03-08 18:42:22 +0100

    Chart view changed. Started to add chart querry.

---
+++ LpgConsumptionCostCalculator.Data/Models/ChartQueryOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LpgConsumptionCostCalculator.Data.Models
+{
+    public class ChartQueryOptions
+    {
+        public ChartQueryOptions()
+        {
+            sortFuelType = TypeOfFuel.LPG;
+            startingTimeRange = DateTime.Now.AddMonths(-1);
+        }
+        public TypeOfFuel sortFuelType { get; set; }
+        public DateTime startingTimeRange { get; set; }
+    }
+}
