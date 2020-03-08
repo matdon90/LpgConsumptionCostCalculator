1ad8d3bbde4a016db8fd12033606433a4eace6a8
Matis <Matis@DESKTOP-U37SB43>
2020-03-08 18:42:22 +0100

    Chart view changed. Started to add chart querry.

---
+++ LpgConsumptionCostCalculator.Web/ViewModels/ChartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LpgConsumptionCostCalculator.Web.ViewModels
+{
+    public class ChartViewModel
+    {
+        public DateTime[] dateTimesArray { get; set; }
+        public decimal[] consumptionArray { get; set; }
+        public decimal[] priceArray { get; set; }
+        public decimal averageFuelCons { get; set; }
+        public decimal minFuelCons { get; set; }
+        public decimal maxFuelCons { get; set; }
+        public decimal avgPrice { get; set; }
+        public decimal totalPrice { get; set; }
+    }
+}
