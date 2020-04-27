295ee05a8c70aa93a50185ee54496e1e1fb69f3a
matdon90 <mdonhefner@gmail.com>
2020-04-27 15:29:57 +0200

    Implemented PDF creating functionality - changes in controller, preparing view, optimizing pdf layout.

---
+++ LpgConsumptionCostCalculator.Web/ViewModels/ExportPdfViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LpgConsumptionCostCalculator.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LpgConsumptionCostCalculator.Web.ViewModels
+{
+    public class ExportPdfViewModel
+    {
+        public string ReportAuthor { get; set; }
+        public string CarData { get; set; }
+        public string RefuelingDate { get; set; }
+        public string PetrolStationName { get; set; }
+        public string FuelType { get; set; }
+        public string FuelPrice { get; set; }
+        public string FuelAmount { get; set; }
+        public string DistanceFromLastRefueling { get; set; }
+        public string FuelConsumption { get; set; }
+        public string PriceFor100km { get; set; }
+        public string AverageFuelCons { get; set; }
+        public string MinFuelCons { get; set; }
+        public string MaxFuelCons { get; set; }
+        public string AvgPrice { get; set; }
+        public string TotalPrice { get; set; }
+        public string TotalDistance { get; set; }
+    }
+}
