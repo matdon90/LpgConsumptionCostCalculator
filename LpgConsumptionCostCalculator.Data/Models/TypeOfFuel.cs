9591436e19cfd54fb004c99587ed7b40ddb272e5
Matis <Matis@DESKTOP-U37SB43>
2020-07-07 14:30:21 +0200

    Implemented sorting, searching and pagination for logs.

--- LpgConsumptionCostCalculator.Data/Models/TypeOfFuel.cs
+++ LpgConsumptionCostCalculator.Data/Models/TypeOfFuel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LpgConsumptionCostCalculator.Data.Models
+﻿namespace LpgConsumptionCostCalculator.Data.Models
 {
     public enum TypeOfFuel
     {
--- LpgConsumptionCostCalculator.Web/Controllers/LoggingController.cs
+++ LpgConsumptionCostCalculator.Web/Controllers/LoggingController.cs
@@ -1,6 +1,12 @@
-﻿using LpgConsumptionCostCalculator.Data.Services;
+﻿using LpgConsumptionCostCalculator.Data.Models;
+using LpgConsumptionCostCalculator.Data.Services;
+using LpgConsumptionCostCalculator.Web.Behaviors;
 using System.Threading.Tasks;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
+using System.Linq;
+using System.Linq.Dynamic;
+using System;
 
 namespace LpgConsumptionCostCalculator.Web.Controllers
 {
@@ -14,11 +20,22 @@ namespace LpgConsumptionCostCalculator.Web.Controllers
         }
 
         [Authorize(Users = "Mateusz Donhefner")]
-        public async Task<ActionResult> Logs()
+        [HttpGet]
+        public async Task<ActionResult> Logs([Form] QueryOptions queryOptions, string searchString)
         {
-            var logs = await _db.GetUsersData();
+            ViewBag.QueryOptions = queryOptions;
+            var start = QueryOptionsCalculator.CalculateStartPage(queryOptions);
+            var logsModel = await _db.GetUsersData();
+            ViewBag.NumberOfEntries = logsModel.Count();
 
-            return View(logs);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.ToUpper();
+                logsModel = logsModel.Where(l => l.UserName.ToUpper().Contains(searchString) || l.LogTime.ToString().Contains(searchString) || l.LogMessage.ToUpper().Contains(searchString));
+            }
+            queryOptions.TotalPages = QueryOptionsCalculator.CalculateTotalPages(logsModel.Count(), queryOptions.PageSize);
+
+            return View(logsModel.OrderBy(queryOptions.Sort).Skip(start).Take(queryOptions.PageSize).ToList());
         }
     }
 }
